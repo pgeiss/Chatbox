138b4c4b23f8f4278acc41feab0b12b540fea5ef
pgeiss <peter.w.geiss@gmail.com>
2015-01-28 17:32:27 -0500

    Node.js stuff
    Just adding some libraries (to be specific, express, mongoDB, and
    monogskin to work with the DB)

--- js/signInPageJS.js
+++ js/signInPageJS.js
@@ -1,7 +1,3 @@
 function requestLogIn() {
-    var inputPair = {
-        username: document.getElementById("inputUsername"),
-        password: document.getElementById("inputPassword")
-    };
-    console.log("Username: ", inputPair.username, " Password: ", inputPair.password);
+
 }
