897f5571ad9c02d8e16307fb37eb30a4fa09d4dc
pgeiss <peter.w.geiss@gmail.com>
2015-01-28 17:43:41 -0500

    Let's try that again
    This time without the node_modules folder.

--- js/signInPageJS.js
+++ js/signInPageJS.js
@@ -3,5 +3,4 @@ function requestLogIn() {
         username: document.getElementById("inputUsername"),
         password: document.getElementById("inputPassword")
     };
-    console.log("Username: ", inputPair.username, " Password: ", inputPair.password);
 }
