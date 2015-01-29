2e214726769e0b976ac5440f9e9884a9965a8501
pgeiss <peter.w.geiss@gmail.com>
2015-01-28 21:09:18 -0500

    Checking something

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
-	res.send("Hello!");
+	res.send('index.html');
 });
 
 app.listen(app.get('port'), function() {
