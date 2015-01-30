fed4263461a2d93e15c2e4024458741c1226d873
pgeiss <peter.w.geiss@gmail.com>
2015-01-29 20:21:33 -0500

    Another one

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function (req, res) {
-	res.sendFile('./index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running on port:" + app.get('port'));
