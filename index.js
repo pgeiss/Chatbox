f8720487552be3024feccbc1e0980687ea069dbf
pgeiss <peter.w.geiss@gmail.com>
2015-01-29 20:36:02 -0500

    __dirname is how you do it.

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
 app.get('/', function (req, res) {
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app is running on port:" + app.get('port'));
 });
