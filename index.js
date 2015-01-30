8f9147a760ce1260ee3b2ce2e805a21614b2919d
pgeiss <peter.w.geiss@gmail.com>
2015-01-30 17:51:10 -0500

    It was 'readdir' not 'readDir'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-	res.send(fs.readDirSync(__dirname));
+	res.send(fs.readdirSync(__dirname));
 });
 
 app.get('css/', function (req, res) {
