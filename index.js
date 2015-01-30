55b14db383b0bebb9b92db9718f801068267866f
pgeiss <peter.w.geiss@gmail.com>
2015-01-30 17:59:03 -0500

    Removing debugging

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-	res.send(fs.readdirSync('/'));
-});
-
-app.get('css/', function (req, res) {
 	res.sendFile(__dirname + req.url);
 });
+
+app.get('/css/', function (req, res) {
+	res.sendFile(__dirname + req.url);
+});
+
+app.get('/js/', function (req, res) {
+	res.sendFile(__dirname = req.url);
+})
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running on port:" + app.get('port'));
