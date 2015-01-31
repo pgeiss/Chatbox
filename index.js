db9018f5bb37d19d103ed42bdc4d76ac3a16fb39
pgeiss <peter.w.geiss@gmail.com>
2015-01-31 17:51:45 -0500

    We need a page for the actual chatting after all

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ app.get('/', function (req, res) {
 	res.sendFile(req.url);
 });
 
-app.get('/css', function (req, res) {
-	res.sendFile(req.url);
-});
-
 app.listen(app.get('port'), function () {
   console.log("Node app is running on port:" + app.get('port'));
 });
