bf66a33e7679839d8217e80170b7fafa4d1e2c06
pgeiss <peter.w.geiss@gmail.com>
2015-01-30 19:20:31 -0500

    Viewing files

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-	res.send(__dirname + '/css');
+	res.send(fs.readdirSync(__dirname + '/css');
 });
 
 app.get('/css/', function (req, res) {
