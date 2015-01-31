68930a2728ae8aa72e7716139cad25ca5d8a1163
pgeiss <peter.w.geiss@gmail.com>
2015-01-30 19:26:35 -0500

    Testing something

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 var express = require('express');
-var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-	res.send(fs.readdirSync(__dirname + '/css'));
+	res.sendFile(req.url);
 });
 
-app.get('/css/', function (req, res) {
-	res.sendFile(__dirname + req.url);
+app.get('/css', function (req, res) {
+	res.sendFile(req.url);
 });
-
-app.get('/js/', function (req, res) {
-	res.sendFile(__dirname = req.url);
-})
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running on port:" + app.get('port'));
