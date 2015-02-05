f9b6dc5c26f858355e7014edbcaae5806233e667
pgeiss <peter.w.geiss@gmail.com>
2015-02-04 20:09:52 -0500

    Forms are POSTable

--- js/registerPageJS.js
+++ js/registerPageJS.js
@@ -1,12 +1,6 @@
 $(document).ready(function () {
     $('#user-pw-form').ajaxForm( {
         type: 'POST',
-        beforeSubmit: preCall,
-        success: console.log("Success"),
         dataType: 'json'
     })
 });
-
-function preCall(formData) {
-    alert('' + formData);
-}
