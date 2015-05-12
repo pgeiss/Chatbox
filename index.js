33a4426348dc830473dca7c0efae83c1cd97638c
pgeiss <peter.w.geiss@gmail.com>
2015-05-12 01:49:50 -0400

    Gonna need to merge here...

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var nodeServer = require('http').Server(app);
 //var mongo = require('mongodb');
 //var database = require('./database.js');
 var io = require('socket.io').listen(nodeServer);
+var clients = [];
 app.set('socketPort', 39000);
 app.set('port', 80);
 app.use(express.static(__dirname));
@@ -13,19 +14,37 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-	console.log("Someone connected.");
+	console.log('A user connected to ID ' + socket.id);
+	clients.push(socket);
+
 	socket.on('msg', function (msg) {
 		console.log('Incoming message: ' + msg);
 		socket.broadcast.emit('incoming message', msg);
 	});
+
+	socket.on('private msg', function (pmsg) {
+		console.log('Incoming pm: ' + pmsg);
+		//TODO
+	})
+
 	socket.on('disconnect', function () {
-		console.log('Someone disconnected.');
+		var toRemove = clients.indexof(socket);
+		if (index != -1) {
+			clients.splice(index, 1);
+			console.log('A user disconnected from ID ' + socket.id);
+		}
 	});
 })
 
-/*app.post('/', function (req, res) {
+app.post('/signin', function (req, res) {
 	console.log("Post request received");
-});*/
+	console.log("UN: " + req.username + " PW: " + req.password);
+});
+
+app.post('/register', function (req, res) {
+	console.log("Post request received");
+	console.log("UN: " + req.username + " PW: " + req.password);
+});
 
 app.listen(app.get('port'), function () {
   console.log("Node app is running on port:" + app.get('port'));
